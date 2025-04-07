8ab3b6abf94d4136736b9a93425f03f098bd7784
CrazyBoy <65382951+CrazyBoy621@users.noreply.github.com>
2025-04-07 21:03:47 +0500

    Initial Commit

---
+++ SwiftUILearning/SwiftUILearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILearningApp.swift
+//  SwiftUILearning
+//
+//  Created by Shohjahon Rakhmatov on 07/04/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
